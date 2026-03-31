00db5d26114d2ba52b4d2fe3d8d42df3533148e6
itouri <hechima.itouri@gmail.com>
2026-03-31 19:18:17 +0900

    3/31

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+// src/components/Header.tsx
+import React from "react";
+
+export const Header: React.FC = () => (
+  <header className="header">
+    <h1>☕ コーヒー自己紹介カードメーカー</h1>
+  </header>
+);
---
+++ src/components/Preview/PewviewSection.tsx
@@ -0,0 +1,5 @@
+export const PreviewSection: React.FC = () => (
+    // プレビューのカード表示の方法
+);
+
+export default PreviewSection;
